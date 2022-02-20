d8d5e39c45c731d7a70d801fb928922a9d12e36c
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-21 03:47:53 +0530

    Changes

--- plugins/inline.py
+++ plugins/inline.py
@@ -46,7 +46,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/SRTheProgrammer/SuraVCPlayBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/SRTheProgrammer/SuraVCPlayBot).</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )

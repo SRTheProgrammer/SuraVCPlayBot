e159a005ce9b5ee150f4ee703619b03b71066f23
SRTheProgrammer <99089545+SRTheProgrammer@users.noreply.github.com>
2022-02-21 04:20:40 +0530

    Changes

--- plugins/commands.py
+++ plugins/commands.py
@@ -196,7 +196,7 @@ async def repo_(client, message):
             InlineKeyboardButton('Close ❌', callback_data='close'),
         ]
     ]
-    await message.reply("<b>The source code of this bot is public and can be found at <a href=https://github.com/ZauteKm/VCPlayerBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.\n\nFeel free to star☀️ the repo if you liked it 🙃.</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply("<b>The source code of this bot is public and can be found at <a href=https://github.com/SRTheProgrammer/SuraVCPlayBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.\n\nFeel free to star☀️ the repo if you liked it 🙃.</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
     await delete_messages([message])
 
 @Client.on_message(filters.command(['restart', 'update', f"restart@{Config.BOT_USERNAME}", f"update@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
